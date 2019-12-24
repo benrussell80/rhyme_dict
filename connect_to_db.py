a6a31cd4f39f5c1d008a3ff46d5a584ff182d504
benrussell80 <bprussell80@gmail.com>
2019-12-24 08:33:13 -0800

    CMD interface to database, updated requirements

---
+++ connect_to_db.py
@@ -0,0 +1,31 @@
+import cmd
+import getpass
+from neo4j import GraphDatabase
+from string import Template
+import os
+
+
+URI = os.getenv("NEO4J_URI")
+USERNAME = os.getenv("NEO4J_USERNAME")
+PASSWORD = os.getenv("NEO4J_PASSWORD")
+
+driver = GraphDatabase.driver(URI, auth=(USERNAME, PASSWORD))
+
+def execute(query):
+    with driver.session() as session:
+        return session.run(query)
+
+class Cmd(cmd.Cmd):
+    intro = str(Template('Connected to $USERNAME@$URI').substitute(USERNAME=USERNAME, URI=URI))
+    prompt = Template('($USERNAME) ').substitute(USERNAME=USERNAME)
+
+    def do_run(self, query):
+        "Run a query in the database."
+        result = execute(query)
+        print(result.data())
+        return
+        
+
+if __name__ == "__main__":
+    Cmd().cmdloop()
+    
